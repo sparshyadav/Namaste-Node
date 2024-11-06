214fd0b745f894e238450916d24240be99134ba8
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-11-06 09:27:14 +0530

    CRUD on Database

---
+++ Database.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://sparshyadavmrt:newpassword@cluster0.ajmb4.mongodb.net/"
+
+const client = new MongoClient(url);
+
+const dbName = "HelloWorld";
+
+async function main() {
+    await client.connect();
+    console.log("Connected Successfully to Server");
+    const db = client.db(dbName);
+    const collection = db.collection("User");
+
+    // Read
+    const findResult=await collection.find({}).toArray();
+    console.log("Found Documents =>", findResult);
+
+    // Insert 
+    const data={
+        firstName: "Sparsh",
+        LastName: "Yadav",
+        city: "Meerut",
+        country: "India"
+    }
+
+    const insertResult=await collection.insertMany([data]);
+    console.log("Inerted Documents =>", insertResult);
+
+    return "Done.";
+}
+
+main()
+    .then(console.log())
+    .catch(console.error)
+    .finally(() => client.close());
